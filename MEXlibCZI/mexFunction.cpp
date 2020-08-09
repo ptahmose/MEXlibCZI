a8fa259a9d98ec0cf4fb8aed776a47a092e82125
Juergen Bohl <jbohl@h-quer.de>
2020-08-09 23:44:20 +0200

    ...

--- MEXlibCZI/mexFunction.cpp
+++ MEXlibCZI/mexFunction.cpp
@@ -5,8 +5,31 @@
 void
 mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 {
+    if (nrhs < 1)
+    {
+        mexErrMsgIdAndTxt("MATLAB:MEXlibCZI:invalidNumInputs", "One input required.");
+    }
+
+    /* input must be a string */
+    if (mxIsChar(prhs[0]) != 1)
+    {
+        mexErrMsgIdAndTxt("MATLAB:MEXlibCZI:inputNotString", "Input must be a string.");
+    }
+
+    /* input must be a row vector */
+    if (mxGetM(prhs[0]) != 1)
+    {
+        mexErrMsgIdAndTxt("MATLAB:MEXlibCZI:inputNotVector", "Input must be a row vector.");
+    }
+
+    /* copy the string data from prhs[0] into a C string input_ buf.    */
+    char* input_buf = mxArrayToUTF8String(prhs[0]);
 
     //plhs[0] = 
     OutputDebugStringA("HELLO WORLD!");
+
+    mxFree(input_buf);
+
+    plhs[0] = mxCreateString("THIS IS A STRING");
     return;
 }
