fa18fa17ad7829ed0bf2983a4e3b094d9dd54a10
Juergen Bohl <jbohl@h-quer.de>
2020-08-17 02:04:29 +0200

    update

--- MEXlibCZI/src/CziInstanceManager.cpp
+++ MEXlibCZI/src/CziInstanceManager.cpp
@@ -12,35 +12,34 @@ using namespace std;
 void CziReaderManager::AddInstance(int id)
 {
     //VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::AddInstance: add instance %" MINTFMT ".", id));
-    std::unique_lock<std::mutex> lck(this->mutex, std::defer_lock);
+    std::unique_lock<std::mutex> lck(this->mutex);
     this->map.insert(make_pair(id, make_shared< CziReader>()));
 }
 
 bool CziReaderManager::RemoveInstance(int id)
 {
     //VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::RemoveInstance: remove instance %" MINTFMT ".", id));
-    std::unique_lock<std::mutex> lck(this->mutex, std::defer_lock);
-    auto noOfElementsRemoved = this->map.erase(id);
+    std::unique_lock<std::mutex> lck(this->mutex);
+    const auto noOfElementsRemoved = this->map.erase(id);
     return noOfElementsRemoved > 0;
 }
 
 std::shared_ptr<CziReader> CziReaderManager::GetInstance(int id)
 {
-    std::unique_lock<std::mutex> lck(this->mutex, std::defer_lock);
+    std::unique_lock<std::mutex> lck(this->mutex);
     return this->map.at(id);
 }
 
 int CziReaderManager::CreateNewInstance()
 {
-    //int id = this->instanceCounter++;
-    int id = this->instanceCounter.fetch_add(1);
+    const int id = this->instanceCounter.fetch_add(1);
     this->AddInstance(id);
     return id;
 }
 
 int CziReaderManager::RemoveAllInstances()
 {
-    std::unique_lock<std::mutex> lck(this->mutex, std::defer_lock);
+    std::unique_lock<std::mutex> lck(this->mutex);
     const int noOfExistingInstances = this->map.size();
     this->map.clear();
     return noOfExistingInstances;
