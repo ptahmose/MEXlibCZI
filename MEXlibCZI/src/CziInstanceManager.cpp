bc54bfd42cfacda4898cf47034f5a2aa300560c1
Juergen Bohl <jbohl@h-quer.de>
2020-08-18 05:27:41 +0200

    ...

--- MEXlibCZI/src/CziInstanceManager.cpp
+++ MEXlibCZI/src/CziInstanceManager.cpp
@@ -1,4 +1,5 @@
 #include "CziInstanceManager.h"
+#include "dbgprint.h"
 
 using namespace std;
 
@@ -11,16 +12,16 @@ using namespace std;
 
 void CziReaderManager::AddInstance(int id)
 {
-    //VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::AddInstance: add instance %" MINTFMT ".", id));
+    VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::AddInstance: add instance %i.\n", id));
     std::unique_lock<std::mutex> lck(this->mutex);
     this->map.insert(make_pair(id, make_shared< CziReader>()));
 }
 
 bool CziReaderManager::RemoveInstance(int id)
 {
-    //VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::RemoveInstance: remove instance %" MINTFMT ".", id));
     std::unique_lock<std::mutex> lck(this->mutex);
     const auto noOfElementsRemoved = this->map.erase(id);
+    VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::RemoveInstance: remove instance %i -> %s.", id, noOfElementsRemoved > 0 ? "success" : "failure"));
     return noOfElementsRemoved > 0;
 }
 
@@ -41,6 +42,7 @@ int CziReaderManager::RemoveAllInstances()
 {
     std::unique_lock<std::mutex> lck(this->mutex);
     const int noOfExistingInstances = this->map.size();
+    VDBGPRINT((CDbg::Level::Trace, "CziReaderManager::RemoveAllInstances: # of existing instances is %i.\n", noOfExistingInstances));
     this->map.clear();
     return noOfExistingInstances;
 }
