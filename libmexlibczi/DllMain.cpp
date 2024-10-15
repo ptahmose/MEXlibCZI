ac3e6ce11346e55af4c71b1c9bc9f9c54bc2f5a0
ptahmose <jbohl@h-quer.de>
2024-10-15 04:24:14 +0200

    Improve Windows compatibility and function exports
    Comment out static linking options in CMakeLists.txt.
    Add Windows-specific includes and DllMain definition in DllMain.cpp.
    Define DLLEXPORT macro in exportedfunctions.cpp for cross-compiler compatibility.

--- libmexlibczi/DllMain.cpp
+++ libmexlibczi/DllMain.cpp
@@ -1,3 +1,4 @@
+#if _WIN32
 #include <Windows.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
@@ -23,3 +24,4 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 
     return TRUE;
 }
+#endif
--- libmexlibczi/exportedfunctions.cpp
+++ libmexlibczi/exportedfunctions.cpp
@@ -1,16 +1,22 @@
 #include <include/function.h>
 
-extern "C" __declspec(dllexport) void mexFunction(int nlhs, Parameter plhs[], int nrhs, const Parameter prhs[], IAppExtensionFunctions* app_functions)
+#ifdef __GNUC__
+#define DLLEXPORT __attribute__((visibility("default")))
+#else
+#define DLLEXPORT __declspec(dllexport)
+#endif
+
+extern "C" DLLEXPORT void mexFunction(int nlhs, Parameter plhs[], int nrhs, const Parameter prhs[], IAppExtensionFunctions* app_functions)
 {
     mexlibCZI::mexFunction(nlhs, plhs, nrhs, prhs, app_functions);
 }
 
-extern "C" __declspec(dllexport) void OnInitialize()
+extern "C" DLLEXPORT void OnInitialize()
 {
     mexlibCZI::OnInitialize();
 }
 
-extern "C" __declspec(dllexport) void OnShutdown()
+extern "C" DLLEXPORT void OnShutdown()
 {
     mexlibCZI::OnShutdown();
 }
