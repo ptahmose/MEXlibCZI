9cf2a4651c91fa5531a34194bbc0a7d6391b51fa
ptahmose <jbohl@h-quer.de>
2024-10-15 04:16:14 +0200

    Refactor to use octaveMex and dynamic DLL loading
    Refactored code to replace matlabMex with octaveMex function names.
    Updated Initialize function to dynamically load libmexlibczi.dll
    from the same directory as the MEX file. Added IsDynamicLibraryLoaded
    function to check if the library and its functions are loaded
    successfully. Updated mexFunction to initialize the library only
    once and check if the dynamic library is loaded before proceeding.
    Removed old and commented-out code. Updated g_appExtensionFunctions
    struct to use new octaveMex function names. Added error handling
    to ensure appropriate error messages are displayed if the library
    fails to load.

--- OctaveOct/octavelibczi.c
+++ OctaveOct/octavelibczi.c
@@ -4,117 +4,117 @@
 #include <string.h>
 #include <Windows.h>
 
-static bool matlabMexIsNanOrInfDouble(double value)
+static bool octaveMexIsNanOrInfDouble(double value)
 {
     return mxIsNaN(value) || mxIsInf(value);
 }
 
-static bool matlabMexIsNanOrInfSingle(float value)
+static bool octaveMexIsNanOrInfSingle(float value)
 {
     return mxIsNaN(value) || mxIsInf(value);
 }
 
-static double matlabMexGetInfinityDouble(void)
+static double octaveMexGetInfinityDouble(void)
 {
     return mxGetInf();
 }
 
-static double matlabMexGetNaNDouble(void)
+static double octaveMexGetNaNDouble(void)
 {
     return mxGetNaN();
 }
 
-static void* matlabMexGetData(const Parameter parameter)
+static void* octaveMexGetData(const Parameter parameter)
 {
     return mxGetData((const mxArray*)parameter);
 }
 
-static uint8_t* matlabMexGetUint8s(const Parameter parameter)
+static uint8_t* octaveMexGetUint8s(const Parameter parameter)
 {
     return mxGetUint8s((const mxArray*)parameter);
 }
 
-static int8_t* matlabMexGetInt8s(const Parameter parameter)
+static int8_t* octaveMexGetInt8s(const Parameter parameter)
 {
     return mxGetInt8s((const mxArray*)parameter);
 }
 
-static uint16_t* matlabMexGetUint16s(const Parameter parameter)
+static uint16_t* octaveMexGetUint16s(const Parameter parameter)
 {
     return mxGetUint16s((const mxArray*)parameter);
 }
 
-static int16_t* matlabMexGetInt16s(const Parameter parameter)
+static int16_t* octaveMexGetInt16s(const Parameter parameter)
 {
     return mxGetInt16s((const mxArray*)parameter);
 }
 
-static uint32_t* matlabMexGetUint32s(const Parameter parameter)
+static uint32_t* octaveMexGetUint32s(const Parameter parameter)
 {
     return mxGetUint32s((const mxArray*)parameter);
 }
 
-static int32_t* matlabMexGetInt32s(const Parameter parameter)
+static int32_t* octaveMexGetInt32s(const Parameter parameter)
 {
     return mxGetInt32s((const mxArray*)parameter);
 }
 
-static uint64_t* matlabMexGetUint64s(const Parameter parameter)
+static uint64_t* octaveMexGetUint64s(const Parameter parameter)
 {
     return mxGetUint64s((const mxArray*)parameter);
 }
 
-static int64_t* matlabMexGetInt64s(const Parameter parameter)
+static int64_t* octaveMexGetInt64s(const Parameter parameter)
 {
     return mxGetInt64s((const mxArray*)parameter);
 }
 
-static double* matlabMexGetDoubles(const Parameter parameter)
+static double* octaveMexGetDoubles(const Parameter parameter)
 {
     return mxGetDoubles((const mxArray*)parameter);
 }
 
-static float* matlabMexGetSingles(const Parameter parameter)
+static float* octaveMexGetSingles(const Parameter parameter)
 {
     return mxGetSingles((const mxArray*)parameter);
 }
 
-static bool* matlabMexGetLogicals(const Parameter parameter)
+static bool* octaveMexGetLogicals(const Parameter parameter)
 {
     return (bool*)mxGetLogicals((const mxArray*)parameter);
 }
 
-static bool matlabMexIsNumeric(const Parameter parameter)
+static bool octaveMexIsNumeric(const Parameter parameter)
 {
     return mxIsNumeric((const mxArray*)parameter);
 }
 
-static bool matlabMexIsChar(const Parameter parameter)
+static bool octaveMexIsChar(const Parameter parameter)
 {
     return mxIsChar((const mxArray*)parameter);
 }
 
-static bool matlabMexIsSparse(const Parameter parameter)
+static bool octaveMexIsSparse(const Parameter parameter)
 {
     return mxIsSparse((const mxArray*)parameter);
 }
 
-static bool matlabMexIsStruct(const Parameter parameter)
+static bool octaveMexIsStruct(const Parameter parameter)
 {
     return mxIsStruct((const mxArray*)parameter);
 }
 
-static Parameter matlabMexCreateString(const char* string)
+static Parameter octaveMexCreateString(const char* string)
 {
     return (Parameter)mxCreateString(string);
 }
 
-static void matlabReportErrorAndRaiseSignal(const char* identifier, const char* message)
+static void octaveMexReportErrorAndRaiseSignal(const char* identifier, const char* message)
 {
     mexErrMsgIdAndTxt(identifier, message);
 }
 
-static char* matlabStrDupHostAllocated(const char* string)
+static char* octaveMexStrDupHostAllocated(const char* string)
 {
     size_t len = strlen(string);
     char* msz = (char*)mxMalloc(len + 1);
@@ -123,23 +123,23 @@ static char* matlabStrDupHostAllocated(const char* string)
     return msz;
 }
 
-static Parameter matlabCreateStructArray(size_t ndim, const size_t* dims, int nfields, const char** field_names)
+static Parameter octaveMexCreateStructArray(size_t ndim, const size_t* dims, int nfields, const char** field_names)
 {
     return (Parameter)mxCreateStructArray(ndim, dims, nfields, field_names);
 }
 
-static void matlabSetFieldByNumber(Parameter pa, size_t i, int fieldnum, Parameter value)
+static void octaveMexSetFieldByNumber(Parameter pa, size_t i, int fieldnum, Parameter value)
 {
     mxSetFieldByNumber((mxArray*)pa, i, fieldnum, (mxArray*)value);
 }
 
-static char* matlabConvertToUTF8String(const Parameter parameter)
+static char* octaveMexConvertToUTF8String(const Parameter parameter)
 {
     //return mxArrayToUTF8String((const mxArray*)parameter);
     return mxArrayToString((const mxArray*)parameter);
 }
 
-static void matlabFree(void* ptr)
+static void octaveMexFree(void* ptr)
 {
     mxFree(ptr);
 }
@@ -175,13 +175,13 @@ static mxClassID AppExtensionClassIdToMxClassId(enum AppExtensionClassId class_i
     }
 }
 
-static Parameter matlabCreateNumericMatrixReal(size_t m, size_t n, enum AppExtensionClassId class_id)
+static Parameter octaveMexCreateNumericMatrixReal(size_t m, size_t n, enum AppExtensionClassId class_id)
 {
     mxClassID mx_class_id = AppExtensionClassIdToMxClassId(class_id);
     return (Parameter)mxCreateNumericMatrix(m, n, mx_class_id, mxREAL);
 }
 
-static enum AppExtensionClassId matlabGetClassId(const Parameter parameter)
+static enum AppExtensionClassId octaveMexGetClassId(const Parameter parameter)
 {
     mxClassID mx_class_id = mxGetClassID((const mxArray*)parameter);
     switch (mx_class_id)
@@ -213,17 +213,17 @@ static enum AppExtensionClassId matlabGetClassId(const Parameter parameter)
     }
 }
 
-static size_t matlabGetNumberOfElements(const Parameter parameter)
+static size_t octaveMexGetNumberOfElements(const Parameter parameter)
 {
     return mxGetNumberOfElements((const mxArray*)parameter);
 }
 
-static size_t matlabGetNumberOfDimensions(const Parameter parameter)
+static size_t octaveMexGetNumberOfDimensions(const Parameter parameter)
 {
     return mxGetNumberOfDimensions((const mxArray*)parameter);
 }
 
-static void matlabGetSizeOfDimensions(const Parameter parameter, size_t number_of_dimension, size_t* sizes)
+static void octaveMexGetSizeOfDimensions(const Parameter parameter, size_t number_of_dimension, size_t* sizes)
 {
     const size_t* ptr_sizes = mxGetDimensions((const mxArray*)parameter);
     for (size_t i = 0; i < number_of_dimension; ++i)
@@ -232,9 +232,9 @@ static void matlabGetSizeOfDimensions(const Parameter parameter, size_t number_o
     }
 }
 
-static Parameter matlabGetField(const Parameter parameter, const char* field_name)
+static Parameter octaveMexGetField(const Parameter parameter, const char* field_name)
 {
-    if (!matlabMexIsStruct(parameter))
+    if (!octaveMexIsStruct(parameter))
     {
         return NULL;
     }
@@ -242,7 +242,7 @@ static Parameter matlabGetField(const Parameter parameter, const char* field_nam
     return (Parameter)mxGetField((const mxArray*)parameter, 0, field_name);
 }
 
-static Parameter matlabCreateNumericArrayReal(size_t ndim, const size_t* dims, enum AppExtensionClassId class_id)
+static Parameter octaveMexCreateNumericArrayReal(size_t ndim, const size_t* dims, enum AppExtensionClassId class_id)
 {
     mxClassID mx_class_id = AppExtensionClassIdToMxClassId(class_id);
     return (Parameter)mxCreateNumericArray(ndim, dims, mx_class_id, mxREAL);
@@ -250,57 +250,41 @@ static Parameter matlabCreateNumericArrayReal(size_t ndim, const size_t* dims, e
 
 static struct IAppExtensionFunctions g_appExtensionFunctions =
 {
-    .pfn_IsNanOrInfDouble = matlabMexIsNanOrInfDouble,
-    .pfn_IsNanOrInfSingle = matlabMexIsNanOrInfSingle,
-    .pfn_GetInfDouble = matlabMexGetInfinityDouble,
-    .pfn_GetNaNDouble = matlabMexGetNaNDouble,
-    .pfn_GetData = matlabMexGetData,
-    .pfn_GetUint8s = matlabMexGetUint8s,
-    .pfn_GetInt8s = matlabMexGetInt8s,
-    .pfn_GetUint16s = matlabMexGetUint16s,
-    .pfn_GetInt16s = matlabMexGetInt16s,
-    .pfn_GetUint32s = matlabMexGetUint32s,
-    .pfn_GetInt32s = matlabMexGetInt32s,
-    .pfn_GetUint64s = matlabMexGetUint64s,
-    .pfn_GetInt64s = matlabMexGetInt64s,
-    .pfn_GetDoubles = matlabMexGetDoubles,
-    .pfn_GetSingles = matlabMexGetSingles,
-    .pfn_GetLogicals = matlabMexGetLogicals,
-    .pfn_IsNumeric = matlabMexIsNumeric,
-    .pfn_IsChar = matlabMexIsChar,
-    .pfn_IsSparse = matlabMexIsSparse,
-    .pfn_IsStruct = matlabMexIsStruct,
-    .pfn_CreateString = matlabMexCreateString,
-    .pfn_ReportErrorAndRaiseSignal = matlabReportErrorAndRaiseSignal,
-    .pfn_StrDupHostAllocated = matlabStrDupHostAllocated,
-    .pfn_CreateStructArray = matlabCreateStructArray,
-    .pfn_SetFieldByNumber = matlabSetFieldByNumber,
-    .pfn_ConvertToUTF8String = matlabConvertToUTF8String,
-    .pfn_Free = matlabFree,
-    .pfn_CreateNumericMatrixReal = matlabCreateNumericMatrixReal,
-    .pfn_GetClassId = matlabGetClassId,
-    .pfn_GetNumberOfElements = matlabGetNumberOfElements,
-    .pfn_GetNumberOfDimensions = matlabGetNumberOfDimensions,
-    .pfn_GetSizeOfDimensions = matlabGetSizeOfDimensions,
-    .pfn_GetField = matlabGetField,
-    .pfn_CreateNumericArrayReal = matlabCreateNumericArrayReal
+    .pfn_IsNanOrInfDouble = octaveMexIsNanOrInfDouble,
+    .pfn_IsNanOrInfSingle = octaveMexIsNanOrInfSingle,
+    .pfn_GetInfDouble = octaveMexGetInfinityDouble,
+    .pfn_GetNaNDouble = octaveMexGetNaNDouble,
+    .pfn_GetData = octaveMexGetData,
+    .pfn_GetUint8s = octaveMexGetUint8s,
+    .pfn_GetInt8s = octaveMexGetInt8s,
+    .pfn_GetUint16s = octaveMexGetUint16s,
+    .pfn_GetInt16s = octaveMexGetInt16s,
+    .pfn_GetUint32s = octaveMexGetUint32s,
+    .pfn_GetInt32s = octaveMexGetInt32s,
+    .pfn_GetUint64s = octaveMexGetUint64s,
+    .pfn_GetInt64s = octaveMexGetInt64s,
+    .pfn_GetDoubles = octaveMexGetDoubles,
+    .pfn_GetSingles = octaveMexGetSingles,
+    .pfn_GetLogicals = octaveMexGetLogicals,
+    .pfn_IsNumeric = octaveMexIsNumeric,
+    .pfn_IsChar = octaveMexIsChar,
+    .pfn_IsSparse = octaveMexIsSparse,
+    .pfn_IsStruct = octaveMexIsStruct,
+    .pfn_CreateString = octaveMexCreateString,
+    .pfn_ReportErrorAndRaiseSignal = octaveMexReportErrorAndRaiseSignal,
+    .pfn_StrDupHostAllocated = octaveMexStrDupHostAllocated,
+    .pfn_CreateStructArray = octaveMexCreateStructArray,
+    .pfn_SetFieldByNumber = octaveMexSetFieldByNumber,
+    .pfn_ConvertToUTF8String = octaveMexConvertToUTF8String,
+    .pfn_Free = octaveMexFree,
+    .pfn_CreateNumericMatrixReal = octaveMexCreateNumericMatrixReal,
+    .pfn_GetClassId = octaveMexGetClassId,
+    .pfn_GetNumberOfElements = octaveMexGetNumberOfElements,
+    .pfn_GetNumberOfDimensions = octaveMexGetNumberOfDimensions,
+    .pfn_GetSizeOfDimensions = octaveMexGetSizeOfDimensions,
+    .pfn_GetField = octaveMexGetField,
+    .pfn_CreateNumericArrayReal = octaveMexCreateNumericArrayReal
 };
-
-#if 0
-void
-mexFunction(int nlhs, mxArray* plhs[],
-    int nrhs, const mxArray* prhs[])
-{
-    mexPrintf("Hello, World!\n");
-
-    mexPrintf("I have %d inputs and %d outputs\n", nrhs, nlhs);
-
-    /* Return empty matrices for any outputs */
-    int i;
-    for (i = 0; i < nlhs; i++)
-        plhs[i] = mxCreateDoubleMatrix(0, 0, mxREAL);
-}
-#endif
 
 static bool gIsInitialized = false;
 
@@ -308,41 +292,81 @@ static void(*pfn_OnShutdown)(void);
 static void(*pfn_OnInitialize)(void);
 static void(*pfn_mexFunction)(int nlhs, Parameter plhs[], int nrhs, const Parameter prhs[], struct IAppExtensionFunctions* app_functions);
 
+static const WCHAR DllName[] = L"libmexlibczi.dll";
+
 static void Initialize()
 {
-    HMODULE hModule = LoadLibraryW(L"libmexlibczi.dll");
-    pfn_OnInitialize = (void(__cdecl*)())GetProcAddress(hModule, "OnInitialize");
-    pfn_OnShutdown = (void(__cdecl*)())GetProcAddress(hModule, "OnShutdown");
-    pfn_mexFunction = (void(__cdecl*)(int, Parameter[], int, const Parameter[], struct IAppExtensionFunctions*))GetProcAddress(hModule, "mexFunction");
+    // we try to load the dynamic library containg the mex function here
+    // * we try to load the library from the same folder where this mex file is located
+    // * therefore, we first get the handle of the module of this mex file, use this handle to get the path of the mex file
+    // * and then, we replace the file name with the name of the dynamic library
+
+    HMODULE hModuleOfMexFile;
+    BOOL B = GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCWSTR)&Initialize, &hModuleOfMexFile);
+    if (!B)
+    {
+        mexErrMsgIdAndTxt("MATLAB:mexlibCZI:getModuleHandleFailed", "Failed to get the handle of the module.");
+    }
+
+    const size_t lenthOfPath = 32768;
+    WCHAR* path = (WCHAR*)malloc((lenthOfPath + (sizeof(DllName) / sizeof(DllName[0] + 1))) * sizeof(WCHAR));
+    DWORD len = GetModuleFileNameW(hModuleOfMexFile, path, 32768);
+    if (len == 0 || (len == lenthOfPath && GetLastError() != ERROR_SUCCESS))
+    {
+        free(path);
+        mexErrMsgIdAndTxt("MATLAB:mexlibCZI:getModuleFileNameFailed", "Failed to get the file name of the module.");
+    }
+
+    // Find the last backslash to isolate the directory
+    for (DWORD i = len; i >= 0; i--)
+    {
+        if (path[i] == L'\\' || path[i] == L'/')
+        {
+            wcscpy(path + i + 1, DllName);
+            break;
+        }
+    }
+
+    HMODULE hModule = LoadLibraryW(path);
+    free(path);
+    if (hModule == NULL)
+    {
+        mexErrMsgIdAndTxt("MATLAB:mexlibCZI:loadLibraryFailed", "Failed to load the library.");
+    }
+
+    pfn_OnInitialize = (void(*)())GetProcAddress(hModule, "OnInitialize");
+    pfn_OnShutdown = (void(*)())GetProcAddress(hModule, "OnShutdown");
+    pfn_mexFunction = (void(*)(int, Parameter[], int, const Parameter[], struct IAppExtensionFunctions*))GetProcAddress(hModule, "mexFunction");
+    if (pfn_OnInitialize == NULL || pfn_OnShutdown == NULL || pfn_mexFunction == NULL)
+    {
+        FreeLibrary(hModule);
+        mexErrMsgIdAndTxt("MATLAB:mexlibCZI:getProcAddressFailed", "Failed to get the address of the function.");
+    }
 
     pfn_OnInitialize();
     mexAtExit(pfn_OnShutdown);
 }
 
-/*
-void
-mexFunction(int nlhs, mxArray* plhs[],
-    int nrhs, const mxArray* prhs[])
+static bool IsDynamicLibraryLoaded()
 {
-    mexPrintf("Hello, World!\n");
-
-    mexPrintf("I have %d inputs and %d outputs\n", nrhs, nlhs);
-
-    // Return empty matrices for any outputs
-    int i;
-    for (i = 0; i < nlhs; i++)
-        plhs[i] = mxCreateDoubleMatrix(0, 0, mxREAL);
+    return pfn_OnInitialize != NULL && pfn_OnShutdown != NULL && pfn_mexFunction != NULL;
 }
-*/
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 {
     if (!gIsInitialized)
     {
-        Initialize();
+        // Note: Since 'mexErrMsgIdAndTxt' leaves the mex function with a signal, we need to set the flag before calling it.
+        //       And we want to try loading the DLL only once - if this fails, then we don't want to try again and the
+        //       mex function will remain in an uninitialized and inoperational state.
         gIsInitialized = true;
+        Initialize();
     }
 
+    if (!IsDynamicLibraryLoaded())
+    {
+        mexErrMsgIdAndTxt("MATLAB:mexlibCZI:libraryNotLoaded", "Failed to load the library.");
+    }
 
     pfn_mexFunction(nlhs, (Parameter*)(plhs), nrhs, (const Parameter*)(prhs), &g_appExtensionFunctions);
 }
